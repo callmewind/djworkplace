8c5272670a01db2739f11c438d1f1956db80f2e9
Eduard Martínez <martinez.bernal@gmail.com>
2019-03-20 23:49:53 +0100

    admin

--- staff/admin.py
+++ staff/admin.py
@@ -3,11 +3,18 @@ from .models import *
 
 @admin.register(StaffProfile)
 class  StaffProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('user',)
     list_display = ('user', 'department', 'location',)
     list_select_related = ('user','department', 'location',)
     list_filter = ('department', 'location',)
     ordering = ('user__first_name', 'user__last_name')
     search_fields = ('user__first_name', 'user__last_name', 'user__username', 'user__email')
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+    	return False
 
 
 @admin.register(Department)
