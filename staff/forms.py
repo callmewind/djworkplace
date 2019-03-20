9fcdb614a5305d0280c53e6a73fcf7aa1f7190b5
Eduard Martínez <martinez.bernal@gmail.com>
2019-03-20 23:14:07 +0100

    bootstrap4

--- staff/forms.py
+++ staff/forms.py
@@ -3,5 +3,5 @@ from .models import *
 from django.utils.translation import ugettext_lazy as _
 
 class CalendarFilterForm(forms.Form):
-	department = forms.ModelChoiceField(label=_('department'), queryset=Department.objects.all(), required=False)
-	location = forms.ModelChoiceField(label=_('location'), queryset=Location.objects.all(), required=False)
+	department = forms.ModelChoiceField(label=_('department'), queryset=Department.objects.all(), required=False, empty_label=_('Filter department'))
+	location = forms.ModelChoiceField(label=_('location'), queryset=Location.objects.all(), required=False, empty_label=_('Filter location'))
