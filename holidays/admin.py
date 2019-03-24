1a6952168c006cbfe39b703915137ef3d35bc939
Eduard Martínez <martinez.bernal@gmail.com>
2019-03-24 20:48:28 +0100

    Holidays approval

--- holidays/admin.py
+++ holidays/admin.py
@@ -4,10 +4,12 @@ from .models import *
 @admin.register(Holiday)
 class HolidayAdmin(admin.ModelAdmin):
 	list_select_related = ('user__staffprofile__department', 'user__staffprofile__location')
-	list_display = ('user', 'start', 'end', 'department', 'location',)
-	list_filter = ('user__staffprofile__department',)
+	list_display = ('user', 'start', 'end', 'department', 'location', 'approval_date',)
+	list_filter = ('user__staffprofile__department', 'user__staffprofile__location')
 	search_fields = ('user__first_name', 'user__last_name', 'user__username', 'user__email')
 	date_hierarchy = 'start'
+	raw_id_fields = ('user', 'approved_by',)
+	readonly_fields = ('approval_date',)
 
 	def department(self, obj):
 		return obj.user.staffprofile.department
--- holidays/models.py
+++ holidays/models.py
@@ -1,18 +1,35 @@
 from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 class Holiday(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_('user'), related_name='holidays')
     start = models.DateField(_('start'))
     end = models.DateField(_('end'))
+    approval_date = models.DateTimeField(blank=True, null=True, editable=False)
+    approved_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name=_('approved by'), related_name='holiday_approvals', blank=True, null=True)
 
     def calendar_text(self):
     	return str(self.user)
 
     def __str__(self):
         return "%d %s - %s" % (self.user_id, self.start, self.end)
+
+    def clean(self):
+        super().clean()
+        if not self.user.staffprofile.department:
+            raise ValidationError('%s must be in a department first' % self.user)
+        if self.approved_by:
+            if not self.user.staffprofile.department.managers.filter(pk=self.approved_by.pk).exists():
+                raise ValidationError('%s is not a manager of %s department' % (self.approved_by, self.user.staffprofile.department))
+            if not self.approval_date:
+                self.approval_date = timezone.now()
+        else:
+            self.approval_date = None
+
 
     class Meta:
         ordering = ['-start', '-end']
