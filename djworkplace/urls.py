912cf395ca82a4cccc833b2d056bac8479dc3a21
Eduard Martínez <martinez.bernal@gmail.com>
2019-03-24 12:56:16 +0100

    customize admin

--- djworkplace/urls.py
+++ djworkplace/urls.py
@@ -17,6 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 from django.views.generic.base import RedirectView
+import os
+from django.utils.translation import ugettext as _
+
+company_name = os.environ.get('COMPANY_NAME', '')
+if company_name:
+    admin.site.site_header = _("%s Admin") % company_name
+    admin.site.site_title = _("%s Admin Portal") % company_name
+    admin.site.index_title = _("Welcome to %s Admin Portal") % company_name
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
