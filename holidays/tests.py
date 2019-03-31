7abab204ed5757ecea5a922b7b77867f5cb012fa
Eduard Martinez <edu@chicfy.com>
2019-03-31 20:09:15 +0200

    moar testing

--- holidays/tests.py
+++ holidays/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.conf import settings
 
-# Create your tests here.
+
+class HolidaysTestCase(TestCase):
+
+    fixtures = ['testing-data.json']
+
+    def setUp(self):
+        pass
+
+    def test_holiday_admin(self):
+        c = Client()
+        response = c.post(settings.LOGIN_URL, {'username': settings.DEFAULT_ADMIN_USERNAME, 'password': settings.DEFAULT_ADMIN_PASSWORD})
+        self.assertEqual(response.status_code, 302)
+        response = c.get(reverse('admin:holidays_holiday_changelist'))
+        self.assertEqual(response.status_code, 200)
--- staff/tests.py
+++ staff/tests.py
@@ -17,3 +17,10 @@ class CalendarTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
         response = c.get(reverse('staff:calendar'))
         self.assertEqual(response.status_code, 200)
+
+    def test_staffprofile_admin(self):
+        c = Client()
+        response = c.post(settings.LOGIN_URL, {'username': settings.DEFAULT_ADMIN_USERNAME, 'password': settings.DEFAULT_ADMIN_PASSWORD})
+        self.assertEqual(response.status_code, 302)
+        response = c.get(reverse('admin:staff_staffprofile_changelist'))
+        self.assertEqual(response.status_code, 200)
