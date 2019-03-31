a6c565088ce90581c0750943e38d9315379783c3
Eduard Martinez <edu@chicfy.com>
2019-03-31 19:58:40 +0200

    testinx fixtures

--- staff/tests.py
+++ staff/tests.py
@@ -5,10 +5,13 @@ from django.conf import settings
 # Create your tests here.
 
 class CalendarTestCase(TestCase):
+
+    fixtures = ['testing-data.json']
+
     def setUp(self):
         pass
 
-    def test_animals_can_speak(self):
+    def test_calendar_view(self):
         c = Client()
         response = c.post(settings.LOGIN_URL, {'username': settings.DEFAULT_ADMIN_USERNAME, 'password': settings.DEFAULT_ADMIN_PASSWORD})
         self.assertEqual(response.status_code, 302)
