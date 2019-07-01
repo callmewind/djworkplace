30ab0b46daa2a56f74f286f1abf868ace814ac53
Eduard Martínez <martinez.bernal@gmail.com>
2019-07-01 23:51:52 +0200

    mas cositas

--- holidays/views.py
+++ holidays/views.py
@@ -3,12 +3,24 @@ from .models import *
 from .forms import *
 from django.urls import reverse
 from django.contrib import messages
+from django.utils import timezone
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class CreateVacations(CreateView):
+class CreateVacations(LoginRequiredMixin, CreateView):
     model = Vacation
     form_class = VacationsForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        year = timezone.now().year
+        approved = 0
+        pending_approval = 0
+        #for v in Vacation.objects.filter(user=self.request.user).filter(Q(start__year=year)|Q(end__year=year)):
+            
+        return context
 
     def get_success_url(self):
         return reverse('staff:calendar', args=[self.object.start.year, self.object.start.month])
